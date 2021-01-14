d8826d52b331d04241fce797d65ceed681acfb84
AndiEcker <aecker2@gmail.com>
2021-01-14 20:14:11 +0000

    fixing border window coordinates
    - added widget position offset to the border line instructions in qrcode_widget.kv.
    - fixed also import order at the bottom of qrcode_widget.py for to pass isort checks (make test).

--- src/kivy_garden/qrcode/qrcode_widget.py
+++ src/kivy_garden/qrcode/qrcode_widget.py
@@ -148,7 +148,8 @@ class QRCodeWidget(FloatLayout):
 
 
 if __name__ == '__main__':
-    from kivy.app import runTouchApp
     import sys
+
+    from kivy.app import runTouchApp
     data = str(sys.argv[1:])
     runTouchApp(QRCodeWidget(data=data))
